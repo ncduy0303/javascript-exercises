eb459403c226fe511428af03f021aa88cec4b87a
Duy <caoduy73@gmail.com>
2023-02-21 10:23:21 +0800

    Solve problems 8-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    strProcessed = str.toLowerCase().replace(/[^a-z]/g, '');
+    strReverse = strProcessed.split('').reverse().join('');
+    return strProcessed === strReverse;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    return n < 0 ? "OOPS" : n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
